2cb32259135196b72ebcf0dc2209ed6434897e1b
Nile Okomo <okomonile3@gmail.com>
2021-11-16 21:36:44 +0300

    Print double-digit numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for decimal in range(00, 100):
+for decimal in range(0, 100):
     if decimal == 99:
         print("{}".format(decimal), end='\n')
-    print("{:02}".format(decimal), end=', ')
+    else:
+        print("{:02}".format(decimal), end=', ')

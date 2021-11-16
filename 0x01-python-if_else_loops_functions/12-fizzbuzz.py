9c910d6accbae0b32458aebeba94d3409ebc0c95
Nile Okomo <okomonile3@gmail.com>
2021-11-16 22:28:28 +0300

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for value in range(1, 101):
+        if value % 3 == 0:
+            print("Fizz", end=' ')
+            continue
+        elif value % 5 == 0:
+            if value == 100:
+                print("Buzz", end='')
+            else:
+                print("Buzz", end=' ')
+            continue
+        elif value % 3 == 0 and value % 5 == 0:
+            print("FizzBuzz", end=' ')
+            continue
+        else:
+            print("{}".format(value),  end=' ')
+
+fizzbuzz()
+print("")

9cbc24f2ff58e788213bfc15cf0fd967a3f4f957
Nile Okomo <okomonile3@gmail.com>
2021-11-19 01:15:48 +0300

    Print number of and the list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+argc = len(argv)
+
+if __name__ == '__main__':
+    if argc == 1:
+        print(f"{argc - 1} arguments.")
+    else:
+        print(f"{argc - 1} arguments:")
+
+    index = 1
+    while index < argc:
+        print(f"{index}: {argv[index]}")
+        index += 1

d19fd622d4523ec34197e5857909518780bc0bc5
Nile Okomo <okomonile3@gmail.com>
2021-11-18 23:12:48 +0300

    Fix bug in code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import calculator_1.py as calc
+import calculator_1 as calc
 
 a = 10
 b = 5
 
 if __name__ == '__main__':
-    print("{:d} + {:d} = {:d}".format(calc.add(a, b)))
-    print("{:d} + {:d} = {:d}".format(calc.sub(a, b)))
-    print("{:d} + {:d} = {:d}".format(calc.mul(a, b)))
-    print("{:d} + {:d} = {:d}".format(calc.div(a, b)))
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))

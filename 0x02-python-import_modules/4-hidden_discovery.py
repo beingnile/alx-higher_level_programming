742f1dfc96995d52411ea0f07fedb84fa8031b0b
Nile Okomo <okomonile3@gmail.com>
2021-11-19 03:13:36 +0300

    Fix bug in code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 import hidden_4
 
 
-def vdir(hidden_4):
-    return [x for x in dir(hidden_4) if not x.startswith('__')]
-
-
 if __name__ == '__main__':
+    def vdir(hidden_4):
+        return [x for x in dir(hidden_4) if not x.startswith('__')]
+
+
     print('\n'.join(str(x) for x in vdir(hidden_4)))

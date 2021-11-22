84cf2a6ce36748b609d0b1881b1e7ae940581913
Nile Okomo <okomonile3@gmail.com>
2021-11-23 01:00:51 +0300

    Remove test script from file

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -7,15 +7,4 @@ def divisible_by_2(my_list=[]):
         else:
             some_list.append(False)
 
-    print(my_list)
-    print(some_list)
     return some_list
-
-
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1

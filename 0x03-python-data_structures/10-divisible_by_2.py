2a6ef17d2089104002d8221a994c984d470b6cfe
Nile Okomo <okomonile3@gmail.com>
2021-11-23 00:58:53 +0300

    Print multiples

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,20 @@
 def divisible_by_2(my_list=[]):
     some_list = []
     for index in range(len(my_list)):
-        if my_list[index] & 2 == 0:
+        if my_list[index] % 2 == 0:
             some_list.append(True)
         else:
             some_list.append(False)
 
+    print(my_list)
+    print(some_list)
     return some_list
+
+
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = divisible_by_2(my_list)
+
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1

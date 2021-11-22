8cc0f2ed64470aa9d15683ce99c881dbe3d75534
Nile Okomo <okomonile3@gmail.com>
2021-11-22 21:39:21 +0300

    Clean up code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,11 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx > (len(my_list) - 1):
         return None
     else:
         return my_list[idx]
-
-
-if __name__ == '__main__':
-    element_at(my_list, idx)

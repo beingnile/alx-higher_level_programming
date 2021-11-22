81a66e8b660153c1ef4ac3c1d4488b4b2eed4f21
Nile Okomo <okomonile3@gmail.com>
2021-11-22 22:39:38 +0300

    Make file executable

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return (None)
+        return None
 
-    large_num = my_list[0]
+    bigint = my_list[0]
     for index in range(len(my_list)):
-        if my_list[index] > large_num:
-            large_num = my_list[index]
+        if my_list[index] > bigint:
+            bigint = my_list[index]
 
-    return (large_num)
+    return (bigint)

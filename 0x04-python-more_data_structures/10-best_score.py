79982a9748fa0b9bd2d2599e01c4ff39b8964195
Nile Okomo <okomonile3@gmail.com>
2021-11-25 02:39:28 +0300

    Fix bug in code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or !a_dictionary:
+    if len(a_dictionary) == 0 or a_dictionary is dict:
         return None
 
     retval = list(a_dictionary.keys())[0]

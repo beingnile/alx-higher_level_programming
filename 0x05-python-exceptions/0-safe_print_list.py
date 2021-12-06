aaecdbea98bf5f79350984e739476508742a9fe4
Nile Okomo <okomonile3@gmail.com>
2021-12-06 09:25:59 -0600

    Revert commit -m 76b8beb

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         new_list = my_list[:x]
-        for item in new_list:
-            print(item, end='')
-        print('\n')
+        print(''.join(str(s) for s in new_list))
         return new_list[x - 1]
     except IndexError:
         num = my_list[-1:]

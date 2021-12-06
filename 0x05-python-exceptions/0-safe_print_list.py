f8264c7f35a2adf64e2b5f5b1e550a165f772a7b
Nile Okomo <okomonile3@gmail.com>
2021-12-06 09:22:35 -0600

    Use for loop to print list content

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     try:
         new_list = my_list[:x]
-        print(''.join(str(s) for s in new_list))
+        for item in new_list:
+            print(item, end='')
+        print('\n')
         return new_list[x - 1]
     except IndexError:
         num = my_list[-1:]

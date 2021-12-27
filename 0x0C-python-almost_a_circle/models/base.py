9857dcc5f289ecdf90199d084e602a9d72c26b41
Nile Okomo <okomonile3@gmail.com>
2021-12-27 23:03:31 +0300

    Include documentation for base.py file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
+""" Class Base is the base for other classes in the project """
+
+
 class Base:
+    """The Base for all theother classes in the project"""
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """
+        Initializes the class
+
+        Attributes:
+            id (int): The ID
+        """
         if id is not None:
             self.id = id
         else:
-             Base.__nb_objects += 1
-             self.id = Base.__nb_objects
-
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

4bcba7e875938ea62d578a93896c5520f19b4b9c
Nile Okomo <okomonile3@gmail.com>
2022-01-02 19:27:08 +0300

    Return a json representation of a list of dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import json
 """This module defines a base class for other classes"""
 
 
@@ -19,3 +20,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Args:
+            list_dictionaries (list): A list of dictionaries
+
+        Returns:
+            The json representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

fea2a2389068b3bc9d00aee0f81eafd8fd2baa24
Nile Okomo <okomonile3@gmail.com>
2022-02-28 16:34:35 +0300

    Semistandard fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 // Print first argument
 
 if (process.argv[2] === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }

78fba9aa0dba13233b6eafb8058f3c2e77528266
Nile Okomo <okomonile3@gmail.com>
2022-02-28 16:20:12 +0300

    Refactor code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 // import { argv } from 'process';
 // Check if the length of argv is greater than 2
 
-if (process.argv.length <= 2) {
+if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');

92b095ccbcab30f8e1fb78d5fe6718f629023318
Nile Okomo <okomonile3@gmail.com>
2022-03-01 00:08:33 +0300

    Semistandard fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,15 +2,15 @@
 
 // Set variable for parsed int
 
-let count = parseInt(process.argv[2]);
+const count = parseInt(process.argv[2]);
 
 // Check if argument is NaN
 // If it is, print message, else, print C is fun count times
 
 if (isNaN(count)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < count; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
 }

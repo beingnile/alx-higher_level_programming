f871b699e874d95d6a1710dd1b98fc980f34f673
Nile Okomo <okomonile3@gmail.com>
2022-03-01 01:11:59 +0300

    Add comments

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 
+// Define function for addition
+
 function add (a, b) {
   return a + b;
 }
+
+// Parse arguments and print the result after addition
 
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);

cc00e5eab67e391517d78e61660cb3884cfa954f
Nile Okomo <okomonile3@gmail.com>
2022-03-02 01:22:10 +0300

    Test code without constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,9 +3,9 @@
 // Class 'Square' defines a square
 // and inherits from class 'Rectangle'
 module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
+//  constructor (size) {
+//    super(size, size);
+//  }
 
   charPrint (c) {
     if (c === undefined) {

33597d47e9c11e809f6ed275ac4b7d0335738727
Nile Okomo <okomonile3@gmail.com>
2022-03-04 02:58:39 +0300

    List all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+The script prints states starting with N from the database hbtn_0e_0_usa
+The username. password and database are passed as arguments
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """Run script if name == main"""
+    mydb = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()

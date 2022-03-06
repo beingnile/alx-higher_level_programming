f38052e4ec1a3d1fc2fa226d66bef453dd1f37dc
Nile Okomo <okomonile3@gmail.com>
2022-03-07 01:13:51 +0300

    Use regular expressions for safe filtering

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+The script prints states where the name matches the
+fourth argument from the database hbtn_0e_0_usa
+The username. password and database are passed as arguments
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """Run script if name == main"""
+    mydb = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = mydb.cursor()
+    sql = ("SELECT * FROM states WHERE name REGEXP '{}' ORDER BY id ASC"
+           .format(argv[4]))
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()

d21f62b932c5152795bab30646f8ef720b2321d1
Nile Okomo <okomonile3@gmail.com>
2022-03-18 00:09:29 +0300

    Add data flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST request to URL passed as argument
-curl -sX POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"

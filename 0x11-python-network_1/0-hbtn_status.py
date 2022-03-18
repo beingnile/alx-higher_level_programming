aad0655f3f0e58cc0753e42c91dfc5e33c5ec651
Nile Okomo <okomonile3@gmail.com>
2022-03-19 01:55:02 +0300

    Add Docstrings for documentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status
+
+Constraints:
+    The package urllib must be used
+    No any other package should be imported
+    A with statement must be used
+    The body of the response must be displayed
+    like the example below.
+
+Example:
+    ::
+        $ ./0-hbtn_status.py | cat -e
+        Body response:$
+            - type: <class 'bytes'>$
+            - content: b'OK'$
+            - utf8 content: OK$
+"""
 from urllib.request import urlopen, Request
 
 
 def main():
+    """Make HTTP request to URL"""
     req = Request('https://alx-intranet.hbtn.io/status')
     with urlopen(req) as response:
         content = response.read()

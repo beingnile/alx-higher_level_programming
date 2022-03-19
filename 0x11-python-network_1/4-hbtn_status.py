8fd5c257a2c82131638bc69a28bf75b122822590
Nile Okomo <okomonile3@gmail.com>
2022-03-19 16:48:58 +0300

    Update module documentation

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,20 +1,4 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status
-using the requests package
-
-Constraints:
-    The package `requests` must be used
-    No any other package should be imported
-    The body of the response must be displayed
-    like the example below.
-
-Output example:
-    ::
-        $ ./4-hbtn_status.py | cat -e
-        Body response:$
-            - type: <class 'str'>$
-            - content: OK$
-"""
 import requests
 
 

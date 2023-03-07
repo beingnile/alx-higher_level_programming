b02771c7a5d4d0daab0823e31bd9d5ee0968bbba
Nile Okomo <okomonile3@gmail.com>
2023-03-08 02:29:21 +0300

    Use string format on print fumctions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 for i in range(100):
+    if i == 99:
+        i = str(i)
+        print("{num:s}".format(num=i))
+        break
+
     if i < 10:
         i = '0' + str(i)
-    if i == 99:
-        print(i)
-        break
-    print(i, end=', ')
+    else:
+        i = str(i)
+    print("{num:s}".format(num=i), end=', ')

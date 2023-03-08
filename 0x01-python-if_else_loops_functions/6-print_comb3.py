2b260422bd1fa8aa9fea12bc8d8037b9d03da526
Nile Okomo <okomonile3@gmail.com>
2023-03-08 17:50:03 +0300

    Prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+someList = []
+for i in range(1, 100):
+    if i < 10:
+        i = '0' + str(i)
+    elif i > 10:
+        if i % 10 == i // 10:
+            continue
+    i = str(i)
+    if i == '89':
+        print("{}".format(i))
+        break
+    if ''.join(sorted(i)) in someList:
+        continue
+    else:
+        someList.append(i)
+    print("{}".format(i), end=', ')

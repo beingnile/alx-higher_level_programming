83bc8119a644fdacb6cee9487129d8155b64c58d
Nile Okomo <okomonile3@gmail.com>
2023-03-13 16:44:06 +0300

    Print nothing if there's no value for matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print('')
     for row in matrix:
         for elem in row:
             if elem == row[-1]:

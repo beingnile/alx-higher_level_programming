00980bf1fddeb40f0aa1862ee3807df95379d92e
Nile Okomo <okomonile3@gmail.com>
2023-03-13 16:36:10 +0300

    Print a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix = [[]]):
+    for row in matrix:
+        for elem in row:
+            if elem == row[-1]:
+                print("{:d}".format(elem))
+                break
+            print("{:d}".format(elem), end=" ")

4874fb9e3b9d266e9b67fdd02eae803e67d63b5d
Nile Okomo <okomonile3@gmail.com>
2023-03-14 22:20:21 +0300

    Pycodestyle fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
+    if my_list is None:
         return
     if len(my_list) > 0:
         new_list = my_list[-1::-1]

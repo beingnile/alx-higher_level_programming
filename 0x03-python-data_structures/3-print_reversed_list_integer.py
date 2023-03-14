9bda91fc47706f3ee77a4b7fc23f436b632133d8
Nile Okomo <okomonile3@gmail.com>
2023-03-14 22:18:00 +0300

    Add conditional check for print reversed list integer

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list == []:
+        return
     if len(my_list) > 0:
         new_list = my_list[-1::-1]
         for i in new_list:

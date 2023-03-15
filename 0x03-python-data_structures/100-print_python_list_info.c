5fc644328fbdac4e2ed2abfa74f0e63198002bee
Nile Okomo <okomonile3@gmail.com>
2023-03-16 02:00:35 +0300

    Fix code to meet requirements

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,23 +6,43 @@
  */
 void print_python_list_info(PyObject *p)
 {
-    Py_ssize_t size, i;
-    PyListObject *list;
+	Py_ssize_t size, i;
+	PyObject *item;
 
-    if (!PyList_Check(p)) {
-        fprintf(stderr, "Invalid argument: Not a Python list\n");
-        return;
-    }
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-    list = (PyListObject *)p;
-    size = PyList_Size(p);
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: ", i);
 
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", list->allocated);
-
-    for (i = 0; i < size; i++) {
-        printf("Element %ld: ", i);
-        PyObject_Print(PyList_GetItem(p, i), stdout, 0);
-        printf("\n");
+		if (PyFloat_Check(item))
+			printf("float\n");
+		else if (PyLong_Check(item))
+			printf("int\n");
+		else if (PyUnicode_Check(item))
+			printf("str\n");
+		else if (PyList_Check(item))
+			printf("list\n");
+		else if (PyTuple_Check(item))
+			printf("tuple\n");
+		else if (PyBytes_Check(item))
+			printf("bytes\n");
+		else if (PySet_Check(item))
+			printf("set\n");
+		else if (PyBool_Check(item))
+			printf("bool\n");
+		else if (PyByteArray_Check(item))
+			printf("bytearray\n");
+		else if (PyDict_Check(item))
+			printf("dict\n");
+		else if (PyModule_Check(item))
+			printf("module\n");
+		else if (PyType_Check(item))
+			printf("type\n");
+		else
+			printf("%s\n", Py_TYPE(item)->tp_name);
     }
 }

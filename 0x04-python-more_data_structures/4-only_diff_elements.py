82083509a85690de6e8ffba66b9cd8af7772a010
Nile Okomo <okomonile3@gmail.com>
2023-03-16 02:51:34 +0300

    Return a set of all elements present in only one set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    for i in set_2:
+        if i in set_1:
+            set_1.remove(i)
+            continue
+        set_1.add(i)
+
+    return set_1

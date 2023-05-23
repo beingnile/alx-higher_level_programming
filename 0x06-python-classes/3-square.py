04feb4257755cf87b2fcdd841a9029b64f6ab248
Nile Okomo <okomonile3@gmail.com>
2023-05-23 14:20:19 +0300

    Add default value for square size

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Defines a square of size size"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:

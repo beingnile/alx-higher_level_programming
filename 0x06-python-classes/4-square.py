7c08ac90e91d0e5f3fc10c7ecd396400eba61c34
Nile Okomo <okomonile3@gmail.com>
2023-05-23 14:43:15 +0300

    Implement a property getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+class Square:
+    """Defines a square object"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of the size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Computes the area of the square"""
+        return self.__size * self.__size

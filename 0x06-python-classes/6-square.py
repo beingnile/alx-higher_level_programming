1b3b994c31e716cc8f1ae871f4d21f107fcaa2d1
Nile Okomo <okomonile3@gmail.com>
2023-05-24 17:08:22 +0300

    Fix position validation for positive integers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,6 +32,8 @@ class Square:
         """Set the square position"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):

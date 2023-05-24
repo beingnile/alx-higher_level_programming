8d5fadd54211a98f95809329a2ca0490c2683a14
Nile Okomo <okomonile3@gmail.com>
2023-05-24 16:59:51 +0300

    Print the square at specific position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Defines a square object"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Get the square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the square size"""
+        if not isinstance(value, size):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the square position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the square position"""
+        if isinstance(value, tuple) and len(value) == 2:
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """Returns the current square's area"""
+        return self.size * self.size
+
+    def my_print(self):
+        """Prints the square to stdout with the characters #"""
+        if self.size == 0:
+            print('')
+        for i in range(self.size):
+            if self.position[1] == 0:
+                print(' ' * self.position[0], end='')
+            for j in range(self.size):
+                print("#", end='')
+            print('')

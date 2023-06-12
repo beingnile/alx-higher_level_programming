7ee57b13dd4f863a06a105813bc13366ee7cd281
Nile Okomo <okomonile3@gmail.com>
2023-06-12 15:48:21 +0300

    Check if an object is an instance of a class that inherited from specified class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function inherits_from()"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an instance of a class that
+    inherited from the specified class
+    """
+    return issubclass(obj.__class__, a_class)

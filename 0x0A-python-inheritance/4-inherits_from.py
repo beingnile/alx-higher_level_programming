d2a8572693f05ade906fa8c169bec8f9bbaffbe8
Nile Okomo <okomonile3@gmail.com>
2023-06-12 15:52:19 +0300

    Specify if the obj class is not equal to a_class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """Checks if an object is an instance of a class that
     inherited from the specified class
     """
-    return issubclass(obj.__class__, a_class)
+    return issubclass(obj.__class__, a_class) and obj.__class__ != a_class

8a79ab60e1e0017e78c89f4bccf9582d9a976443
Nile Okomo <okomonile3@gmail.com>
2023-06-12 15:59:19 +0300

    Define an empty class with an unimplemented instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Defines a Base class BaseGeometry"""
+    def area(self):
+        """Unimplemented instance method"""
+        raise Exception("area() is not implemented")

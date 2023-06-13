47a44427d4928565f712d119277518d1ae31c53e
Nile Okomo <okomonile3@gmail.com>
2023-06-13 13:05:21 +0300

    Fix rebel class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,7 +7,7 @@ class MyInt(int):
     and != operators"""
     def __eq__(self, other):
         """Flips operation to !="""
-        return not super().__ne__(other)
+        return not super().__eq__(other)
 
     def __ne__(self, other):
         """"Flips operation to =="""

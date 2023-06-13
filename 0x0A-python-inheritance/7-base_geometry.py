2ac128f0047363e88595224465e8abfaa6ce9c2d
Nile Okomo <okomonile3@gmail.com>
2023-06-13 11:23:07 +0300

    Create simple base class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a baseclass BaseGeometry"""
+
+
+class BaseGeometry:
+    """A base class"""
+    def area(self):
+        """Not yet implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")

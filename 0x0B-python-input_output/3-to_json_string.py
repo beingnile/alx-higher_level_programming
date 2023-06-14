7e34a1c2664ab82e17dd3db9595f3f968f6fe508
Nile Okomo <okomonile3@gmail.com>
2023-06-14 16:57:40 +0300

    Update docstring

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,5 +4,12 @@ import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object (string)"""
+    """Returns the JSON representation of an object (string)
+
+    Args:
+        my_obj(any): An object of any type
+
+    Return:
+        (str): The JSON representation of an object
+    """
     return json.dumps(my_obj)

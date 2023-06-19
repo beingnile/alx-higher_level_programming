5c3cc4cc1553bd40a38f56d33c25d824adaae09f
Nile Okomo <okomonile3@gmail.com>
2023-06-19 12:48:59 +0300

    Update tests for value validations

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -30,3 +30,153 @@ class RectangleTestCase(unittest.TestCase):
         r6 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r6.width, 10)
         self.assertEqual(r6.height, 2)
+
+    def test_str_passed_as_width(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle('10', 2, 0, 0, 12)
+        self.assertEqual('width must be an integer', str(exc.exception))
+
+    def test_none_passed_as_width(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(None, 2, 0, 0, 12)
+        self.assertEqual('width must be an integer', str(exc.exception))
+
+    def test_tuple_passed_as_width(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle((2, 3), 2, 0, 0, 12)
+        self.assertEqual('width must be an integer', str(exc.exception))
+
+    def test_list_passed_as_width(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle([1, 2, 3], 2, 0, 0, 12)
+        self.assertEqual('width must be an integer', str(exc.exception))
+
+    def test_dict_passed_as_width(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle({'width': 10}, 2, 0, 0, 12)
+        self.assertEqual('width must be an integer', str(exc.exception))
+
+    def test_obj_passed_as_width(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(object, 2, 0, 0, 12)
+        self.assertEqual('width must be an integer', str(exc.exception))
+
+    def test_zero_passed_as_width(self):
+        with self.assertRaises(ValueError) as exc:
+            r = Rectangle(0, 2, 0, 0, 12)
+        self.assertEqual('width must be > 0', str(exc.exception))
+
+    def test_neg_val_passed_as_width(self):
+        with self.assertRaises(ValueError) as exc:
+            r = Rectangle(-10, 2, 0, 0, 12)
+        self.assertEqual('width must be > 0', str(exc.exception))
+
+    def test_str_passed_as_height(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, '2', 0, 0, 12)
+        self.assertEqual('height must be an integer', str(exc.exception))
+
+    def test_none_passed_as_height(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, None, 0, 0, 12)
+        self.assertEqual('height must be an integer', str(exc.exception))
+
+    def test_tuple_passed_as_height(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, (2, 3), 0, 0, 12)
+        self.assertEqual('height must be an integer', str(exc.exception))
+
+    def test_list_passed_as_height(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, [1, 2, 3], 0, 0, 12)
+        self.assertEqual('height must be an integer', str(exc.exception))
+
+    def test_dict_passed_as_height(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, {'width': 10}, 0, 0, 12)
+        self.assertEqual('height must be an integer', str(exc.exception))
+
+    def test_obj_passed_as_height(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, object, 0, 0, 12)
+        self.assertEqual('height must be an integer', str(exc.exception))
+
+    def test_zero_passed_as_height(self):
+        with self.assertRaises(ValueError) as exc:
+            r = Rectangle(10, 0, 0, 0, 12)
+        self.assertEqual('height must be > 0', str(exc.exception))
+
+    def test_neg_val_passed_as_height(self):
+        with self.assertRaises(ValueError) as exc:
+            r = Rectangle(10, -2, 0, 0, 12)
+        self.assertEqual('height must be > 0', str(exc.exception))
+
+    def test_str_passed_as_x(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, '0', 0, 12)
+        self.assertEqual('x must be an integer', str(exc.exception))
+
+    def test_none_passed_as_x(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, None, 0, 12)
+        self.assertEqual('x must be an integer', str(exc.exception))
+
+    def test_tuple_passed_as_x(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, (1, 2), 0, 12)
+        self.assertEqual('x must be an integer', str(exc.exception))
+
+    def test_list_passed_as_x(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, [0, 1, 2], 0, 12)
+        self.assertEqual('x must be an integer', str(exc.exception))
+
+    def test_dict_passed_as_x(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, {'x': 0}, 0, 12)
+        self.assertEqual('x must be an integer', str(exc.exception))
+
+    def test_obj_passed_as_x(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, object, 0, 12)
+        self.assertEqual('x must be an integer', str(exc.exception))
+
+    def test_neg_val_passed_as_x(self):
+        with self.assertRaises(ValueError) as exc:
+            r = Rectangle(10, 2, -10, 0, 12)
+        self.assertEqual('x must be >= 0', str(exc.exception))
+
+    def test_str_passed_as_y(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, 0, '0', 12)
+        self.assertEqual('y must be an integer', str(exc.exception))
+
+    def test_none_passed_as_y(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, 0, None, 12)
+        self.assertEqual('y must be an integer', str(exc.exception))
+
+    def test_tuple_passed_as_y(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, 0, (1, 2), 12)
+        self.assertEqual('y must be an integer', str(exc.exception))
+
+    def test_list_passed_as_y(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, 0, [0, 1, 2], 12)
+        self.assertEqual('y must be an integer', str(exc.exception))
+
+    def test_dict_passed_as_y(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, 0, {'x': 0}, 12)
+        self.assertEqual('y must be an integer', str(exc.exception))
+
+    def test_obj_passed_as_y(self):
+        with self.assertRaises(TypeError) as exc:
+            r = Rectangle(10, 2, 0, object, 12)
+        self.assertEqual('y must be an integer', str(exc.exception))
+
+    def test_neg_val_passed_as_y(self):
+        with self.assertRaises(ValueError) as exc:
+            r = Rectangle(10, 2, 0, -10, 12)
+        self.assertEqual('y must be >= 0', str(exc.exception))

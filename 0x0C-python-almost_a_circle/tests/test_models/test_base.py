c116237e6fb78b6ce75c8de54209420f30578351
Nile Okomo <okomonile3@gmail.com>
2023-06-20 16:32:22 +0300

    Write tests for save_to_file method

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Tests the base class"""
+import os
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -7,6 +8,13 @@ from models.rectangle import Rectangle
 
 class BaseTestCase(unittest.TestCase):
     """TestCases for Base class"""
+    def setUp(self):
+        self.filename = 'Rectangle.json'
+
+    def tearDown(self):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
     def test_init_without_arg(self):
         """Test the id of an object when created without args"""
         b1 = Base()
@@ -42,3 +50,15 @@ class BaseTestCase(unittest.TestCase):
         expected = '[{"x": 2, "y": 8, "id": 1, "height": 7, "width": 10}]'
 
         self.assertEqual(json_dict, expected)
+
+    def test_save_to_file(self):
+        self.assertFalse(os.path.exists(self.filename))
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        r2 = Rectangle(2, 4, 0, 0, 2)
+        Rectangle.save_to_file([r1, r2])
+        self.assertTrue(os.path.exists(self.filename))
+        exp = '[{"y": 8, "x": 2, "id": 1, "width": 10, "height": 7}, {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}]'
+        with open(self.filename, 'r') as f:
+            content = f.read()
+
+        self.assertTrue(exp, content)

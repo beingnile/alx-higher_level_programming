c5845e460b4da75806aa9297161fbb83d0d31393
Nile Okomo <okomonile3@gmail.com>
2023-06-20 03:19:19 +0300

    Write getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,20 @@ class Square(Rectangle):
     """Defines a Square object"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
+        self.size = size
+
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = value
 
     def __str__(self):
         """Overwrite __str__ for custom str output"""

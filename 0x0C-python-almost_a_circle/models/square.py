c6659591beee5c5f2bc33933e998c750af02ec2b
Nile Okomo <okomonile3@gmail.com>
2023-06-20 03:41:45 +0300

    Fix getter and setters for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,22 +6,17 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Defines a Square object"""
     def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
         self.size = size
-        super().__init__(width=self.size, height=self.size, x=x, y=y, id=id)
 
     @property
     def size(self):
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__size = value
-        self.__width = self.__size
-        self.__height = self.__size
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Overwrite __str__ for custom str output"""
@@ -29,3 +24,13 @@ class Square(Rectangle):
         ret2 = f'{self.x}/{self.y} - {self.width}'
 
         return ret1 + ret2
+
+    def update(self, *args, **kwargs):
+        """Updates the Square attributes"""
+        attrs = ['id', 'size', 'x', 'y']
+        if args is None or len(args) == 0 and kwargs is not None:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+        for i, value in enumerate(args):
+            setattr(self, attrs[i], value)

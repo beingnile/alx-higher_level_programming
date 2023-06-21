747b17c9472ad35b60fdcc62c217282c9e3d47aa
Nile Okomo <okomonile3@gmail.com>
2023-06-21 18:43:08 +0300

    Fix bug in save_to_file method and implement load_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,12 +19,15 @@ class Base:
         """Writes the JSON string representation of
         list_objs to a file
         """
-        with open(f'{cls.__name__}.json', 'w') as f:
-            if list_objs is None or len(list_objs) == 0:
+        filename = f'{cls.__name__}.json'
+        if list_objs is None or len(list_objs) == 0:
+            with open(filename, 'w') as f:
                 f.write('[]')
-                return
-            for obj in list_objs:
-                f.write(Base.to_json_string([obj.to_dictionary()]))
+        else:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+            list_json = cls.to_json_string(list_dicts)
+            with open(filename, 'w') as f:
+                f.write(list_json)
 
     @classmethod
     def create(cls, **dictionary):
@@ -39,6 +42,23 @@ class Base:
             instance.update(**dictionary)
 
         return instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances"""
+        filename = cls.__name__ + '.json'
+        res = []
+        try:
+            with open(filename, 'r') as f:
+                str_json = f.read()
+                list_json = cls.from_json_string(str_json)
+                for instance in list_json:
+                    ins = cls.create(**instance)
+                    res.append(ins)
+            return res
+        except FileNotFoundError:
+            return res
+
 
     @staticmethod
     def to_json_string(list_dictionaries):

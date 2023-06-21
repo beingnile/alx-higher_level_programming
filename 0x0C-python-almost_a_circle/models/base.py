d335d7f7bc7f8dce0af23388823d36b29cc46a3b
Nile Okomo <okomonile3@gmail.com>
2023-06-21 16:22:10 +0300

    Return a list of the JSON string representation json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,15 +14,6 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Returns the JSON string representation of
-        list_dictionaries
-        """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of
@@ -34,3 +25,19 @@ class Base:
                 return
             for obj in list_objs:
                 f.write(Base.to_json_string([obj.to_dictionary()]))
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of
+        list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+        if json_string is None or json_string == '':
+            return []
+        return json.loads(json_string)

62e967d9b4d2edb348ddd545301dd851867f2d3c
Nile Okomo <okomonile3@gmail.com>
2023-06-21 20:27:29 +0300

    Update base class to handle csv files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a base class to manage the id attribute"""
 import json
+import csv
 
 
 class Base:
@@ -13,6 +14,20 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == 'Rectangle':
+            from models.rectangle import Rectangle
+            instance = Rectangle(1, 1)
+            instance.update(**dictionary)
+        if cls.__name__ == 'Square':
+            from models.square import Square
+            instance = Square(1)
+            instance.update(**dictionary)
+
+        return instance
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -30,23 +45,9 @@ class Base:
                 f.write(list_json)
 
     @classmethod
-    def create(cls, **dictionary):
-        """Returns an instance with all attributes already set"""
-        if cls.__name__ == 'Rectangle':
-            from models.rectangle import Rectangle
-            instance = Rectangle(1, 1)
-            instance.update(**dictionary)
-        if cls.__name__ == 'Square':
-            from models.square import Square
-            instance = Square(1)
-            instance.update(**dictionary)
-
-        return instance
-
-    @classmethod
     def load_from_file(cls):
         """Return a list of instances"""
-        filename = cls.__name__ + '.json'
+        filename = 'cls.__name__.json'
         res = []
         try:
             with open(filename, 'r') as f:
@@ -55,6 +56,43 @@ class Base:
                 for instance in list_json:
                     ins = cls.create(**instance)
                     res.append(ins)
+            return res
+        except FileNotFoundError:
+            return res
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Save objects to csv file"""
+        filename = f'{cls.__name__}.csv'
+        if list_objs is None or len(list_objs) == 0:
+            with open(filename, 'w') as f:
+                if cls.__name__ == 'Rectangle':
+                    fieldnames = ['id', 'width','height', 'x', 'y']
+                else:
+                    fieldnames = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(f, filename)
+                writer.writeheader()
+        else:
+            with open(filename, 'w') as f:
+                if cls.__name__ == 'Rectangle':
+                    fieldnames = ['id', 'width','height', 'x', 'y']
+                else:
+                    fieldnames = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(f, fieldnames)
+                writer.writeheader()
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Load objects from csv file"""
+        filename = f'{cls.__name__}.csv'
+        res = []
+        try:
+            with open(filename, 'r') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    res.append(row)
             return res
         except FileNotFoundError:
             return res

2ae00f43050a1fbea7d65d7ed3c52caa0de390b2
Nile Okomo <okomonile3@gmail.com>
2023-08-13 00:23:26 +0300

    Flush privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- Lists all privileges of the MySQL users user_0d_1
 -- and user_0d_2 on a server
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

7d83c49205cc459a57cb871612234d54087bfd15
Nile Okomo <okomonile3@gmail.com>
2023-08-16 15:18:28 +0300

    Fix semistandard lint errors

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-	const line = 'X'.repeat(size);
-	console.log(line);
+    const line = 'X'.repeat(size);
+    console.log(line);
   }
 }

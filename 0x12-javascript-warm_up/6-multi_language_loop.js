5cc98edb664cc6977a137714b7a2a1dc203f1f12
Nile Okomo <okomonile3@gmail.com>
2023-08-16 14:03:14 +0300

    Write a script that prints 3 lines like 1-multi_languages.js

---
+++ 0x12-javascript-warm_up/6-multi_language_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const argCount = args.length;
+
+for (let i = 0; i < argCount; i++) {
+  console.log(args[i]);
+}

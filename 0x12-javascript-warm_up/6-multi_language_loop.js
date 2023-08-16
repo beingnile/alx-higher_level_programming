9fcc78870e29cd3bd6b954c61703fbbfd676f5d1
Nile Okomo <okomonile3@gmail.com>
2023-08-16 14:04:42 +0300

    Fix file name

--- 0x12-javascript-warm_up/6-multi_language_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-const argCount = args.length;
-
-for (let i = 0; i < argCount; i++) {
-  console.log(args[i]);
-}

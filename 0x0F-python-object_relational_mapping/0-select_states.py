003cef3f5f2a630f9cc0d3561f95cc4a0fee7947
Nile Okomo <okomonile3@gmail.com>
2023-08-20 02:10:01 +0300

    Update script to prevent execution when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,15 +4,16 @@ import MySQLdb
 import sys
 
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
-host = 'localhost'
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    host = 'localhost'
 
-conn = MySQLdb.connect(host=host, port=3306, user=user, passwd=passwd, db=db)
-cur = conn.cursor()
+    conn = MySQLdb.connect(host=host, port=3306, user=user, passwd=passwd, db=db)
+    cur = conn.cursor()
 
-cur.execute('SELECT id, name FROM states ORDER BY id ASC')
-states = cur.fetchall()
-for state in states:
-    print(state)
+    cur.execute('SELECT id, name FROM states ORDER BY id ASC')
+    states = cur.fetchall()
+    for state in states:
+        print(state)

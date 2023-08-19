a95864f51113d2fbc22499e33ed91293ec4c78f8
Nile Okomo <okomonile3@gmail.com>
2023-08-20 02:05:18 +0300

    Write a script that lists all states from the datatbase hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
+host = 'localhost'
+
+conn = MySQLdb.connect(host=host, port=3306, user=user, passwd=passwd, db=db)
+cur = conn.cursor()
+
+cur.execute('SELECT id, name FROM states ORDER BY id ASC')
+states = cur.fetchall()
+for state in states:
+    print(state)

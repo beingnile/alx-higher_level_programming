964100f04f68ae684dbc7e96a7a1a901454cdc63
Nile Okomo <okomonile3@gmail.com>
2023-08-20 02:14:09 +0300

    Fix linter error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,11 @@ import sys
 
 if __name__ == '__main__':
     user = sys.argv[1]
-    passwd = sys.argv[2]
+    pwd = sys.argv[2]
     db = sys.argv[3]
     host = 'localhost'
 
-    conn = MySQLdb.connect(host=host, port=3306, user=user, passwd=passwd, db=db)
+    conn = MySQLdb.connect(host=host, port=3306, user=user, passwd=pwd, db=db)
     cur = conn.cursor()
 
     cur.execute('SELECT id, name FROM states ORDER BY id ASC')

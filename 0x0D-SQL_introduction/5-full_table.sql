2c4124a34330811b44cf1874a63f4f20e3393d79
Nile Okomo <okomonile3@gmail.com>
2023-12-13 19:28:39 -0800

    Add script to describe a table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Shows the full description of a table
+SELECT column_name, data_type, character_maximum_length
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';

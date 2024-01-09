84adc866a295330b17d7a19417febf4b1d9eb1db
Nile Okomo <okomonile3@gmail.com>
2024-01-09 09:05:31 +0300

    Fix array sort issue

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,11 +3,11 @@ const raw = process.argv.slice(2);
 const arr = [];
 
 for (const item of raw) {
-  arr.push(parseInt(item));
+  arr.push(Number(parseInt(item)));
 }
 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  console.log(arr.sort().slice(-2)[0]);
+  console.log(arr.sort((a, b) => a - b).slice(-2)[0]);
 }

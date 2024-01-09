aaa4684acc200b406b3d38329efad37c078a1525
Nile Okomo <okomonile3@gmail.com>
2024-01-09 18:28:20 +0300

    Fix errors in code

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
+const Rectangle = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-}
+};
 
 module.exports = { Rectangle };

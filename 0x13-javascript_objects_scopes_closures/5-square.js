bad3daaf84171d69a3337d005432989c18901ddc
Nile Okomo <okomonile3@gmail.com>
2024-01-09 20:20:52 +0300

    Handle size not an integer

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,6 +4,9 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size);
+    if (isNaN(size) || size <= 0) {
+      return this;
+    }
     this.width = size;
     this.height = size;
   }

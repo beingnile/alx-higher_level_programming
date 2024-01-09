67e556ebe5f04bca692f9df7d35f178bfbfa5078
Nile Okomo <okomonile3@gmail.com>
2024-01-09 19:36:01 +0300

    Fix contructor return

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
-      return {};
+      return this;
     }
     this.width = w;
     this.height = h;

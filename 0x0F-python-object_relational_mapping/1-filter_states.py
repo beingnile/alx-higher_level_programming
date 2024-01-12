8e8dcc7c143d07da48e2ab363fd3c1b076a36f04
Nile Okomo <okomonile3@gmail.com>
2024-01-12 17:36:58 +0300

    Add script to select names from state that start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a function that lists states
+starting with letter N
+"""
+import sys
+import MySQLdb
+
+
+def filter_states(user, passwd, db):
+    """Lists states starting with N"""
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=user, passwd=passwd,
+                           db=db, charset='utf8')
+    cur = conn.cursor()
+    p = 'SELECT * FROM states WHERE name LIKE \'N%\';'
+    cur.execute(p)
+    names = cur.fetchall()
+    for name in names:
+        print(name)
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    filter_states(username, password, db)

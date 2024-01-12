8eb4ab257a31b093fe20e6d5194ca5846a18162c
Nile Okomo <okomonile3@gmail.com>
2024-01-12 20:37:02 +0300

    Make check case-insensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ def filter_states(user, passwd, db):
                            user=user, passwd=passwd,
                            db=db, charset='utf8')
     cur = conn.cursor()
-    p = "SELECT * FROM states WHERE name REGEXP '^N';"
+    p = "SELECT * FROM states WHERE name REGEXP '[^Nn]';"
     cur.execute(p)
     names = cur.fetchall()
     for name in names:

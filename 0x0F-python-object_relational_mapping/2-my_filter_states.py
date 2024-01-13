9e7615f4c315df25f662e7c7681ed95c3fa1f3f1
Nile Okomo <okomonile3@gmail.com>
2024-01-13 12:46:04 +0300

    Add script to list states matching arguments passed

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches
+the argument"""
+import sys
+import MySQLdb
+
+
+def my_filter_states(user, passwd, db, name):
+    """Displays all values where name === name"""
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=user, passwd=passwd,
+                           db=db, charset='utf8')
+    cur = conn.cursor()
+    p = f"SELECT * FROM states WHERE name = '{name}'"
+    cur.execute(p)
+    row = cur.fetchone()
+    print(row)
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    name = sys.argv[4]
+
+    my_filter_states(user, passwd, db, name)

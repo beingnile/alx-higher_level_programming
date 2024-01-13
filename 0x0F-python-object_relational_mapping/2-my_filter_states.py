cdf719db40dcda4fa2af84e8282821e645a9d594
Nile Okomo <okomonile3@gmail.com>
2024-01-13 12:50:40 +0300

    Use format to format string input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ def my_filter_states(user, passwd, db, name):
                            user=user, passwd=passwd,
                            db=db, charset='utf8')
     cur = conn.cursor()
-    p = f"SELECT * FROM states WHERE name = '{name}' ORDER BY id ASC"
+    p = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(name)
     cur.execute(p)
     row = cur.fetchone()
     print(row)

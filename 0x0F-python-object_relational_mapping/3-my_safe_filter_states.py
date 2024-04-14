42e9f348117fc2c5d55e3bcde837059523a48636
Nile Okomo <okomonile3@gmail.com>
2024-04-14 19:23:02 +0300

    Add script to safely print states matching user input

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Prints states matching an argument passed from
+the table states in the database hbtn_0e_0_usa"""
+from sys import argv
+import MySQLdb
+
+
+def filter_states_by_arg(user, passwd, db, arg):
+    """Connects to a MySQL server and executes commands
+    Arguments passed to uery ae parameterized to prevent
+    SQL injection attacks
+    """
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=user,
+                           passwd=passwd,
+                           db=db)
+    cur = conn.cursor()
+    p = "SELECT * FROM states WHERE BINARY name LIKE %s;"
+    cur.execute(p, (arg,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    arg = argv[4]
+
+    filter_states_by_arg(user, passwd, db, arg)

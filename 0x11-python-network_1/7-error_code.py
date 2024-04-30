eed281d6952ce874a0d4ed1eeac3f20de888cf9a
Nile Okomo <okomonile3@gmail.com>
2024-04-30 17:53:36 +0300

    Add script that takes in a URL argument and sends a GET request

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Takes in a URL and sends a GET request
+If status code >= 400, prints 'Error code:'
+followed by the value of the HTTP status code
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status >= 400:
+        print(f"Error code: {r.status}")
+    else:
+        print(r.text)

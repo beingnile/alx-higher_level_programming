c1482dd6b0e3f9f0c8600ae00ea765bb80fc3a10
Nile Okomo <okomonile3@gmail.com>
2024-04-30 16:02:30 +0300

    Include exception handling

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,12 @@ if __name__ == '__main__':
     import urllib.request
 
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        res = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(res)}")
-        print(f"\t- content: {res}")
-        print(f"\t- utf8 content: {response.msg}")
+    try:
+        with urllib.request.urlopen(req) as response:
+            res = response.read()
+            print("Body response:")
+            print(f"\t- type: {type(res)}")
+            print(f"\t- content: {res}")
+            print(f"\t- utf8 content: {response.msg}")
+    except Exception:
+        pass

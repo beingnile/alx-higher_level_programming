85599940f61b2892acda150ddfd96cd9a15929bb
Nile Okomo <okomonile3@gmail.com>
2024-04-30 10:01:51 +0300

    Add python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+if __name__ == '__main__':
+    import urllib.request
+
+
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(res)}")
+        print(f"\t- content: {res}")
+        print(f"\t- utf8 content: {response.msg}")

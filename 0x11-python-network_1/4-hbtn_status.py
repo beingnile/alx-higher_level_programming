509de16e6d8a56c531cd98375689e5839de836a9
Nile Okomo <okomonile3@gmail.com>
2024-04-30 17:15:46 +0300

    Use requests package to fetch a URL

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Fetches https://alx-intranet.hbtn.io/status
+if __name__ == '__main__':
+    import requests
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url).text
+    print("Body response:")
+    print(f"\t- type: {type(res)}")
+    print(f"\t- content: {res}")
